806b75907546c5c7a8b4295f09cb8ec211a5ee79
Rafael Ramirez <randre17@hotmail.com>
2024-08-08 19:09:31 -0500

    initial commit

---
+++ src/test/java/com/example/evaluationjava/EvaluationjavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evaluationjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationjavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
